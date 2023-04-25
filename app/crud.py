daa5343fc22add703d9632feb2bd56e20467054c
h-k-nyosu <kuro.h1020@gmail.com>
2023-04-25 22:32:55 +0900

    wip: アイテムコマを3行に変換

--- app/crud.py
+++ app/crud.py
@@ -1,11 +1,17 @@
 from typing import List
 from .models import Item
 
+
 def get_items() -> List[Item]:
     items = [
-        Item(name="アイテム1", image="https://placehold.jp/300x200.png"),
-        Item(name="アイテム2", image="https://placehold.jp/300x200.png"),
-        Item(name="アイテム3", image="https://placehold.jp/300x200.png"),
-        # 他のアイテムを追加
+        Item(name="アイテム1", image="https://placehold.jp/300x100.png"),
+        Item(name="アイテム2", image="https://placehold.jp/300x100.png"),
+        Item(name="アイテム3", image="https://placehold.jp/300x100.png"),
+        Item(name="アイテム4", image="https://placehold.jp/300x100.png"),
+        Item(name="アイテム5", image="https://placehold.jp/300x100.png"),
+        Item(name="アイテム6", image="https://placehold.jp/300x100.png"),
+        Item(name="アイテム7", image="https://placehold.jp/300x100.png"),
+        Item(name="アイテム8", image="https://placehold.jp/300x100.png"),
+        Item(name="アイテム9", image="https://placehold.jp/300x100.png"),
     ]
     return items
