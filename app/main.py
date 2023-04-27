a78d20db6413eb1cd826da310952f2b06ef7c9f2
h-k-nyosu <kuro.h1020@gmail.com>
2023-04-27 14:55:30 +0900

    wip: streaming機能追加

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
+import queue
+import threading
 from fastapi import FastAPI, Request, Depends, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.schema import (
+    HumanMessage,
+    SystemMessage
+)
+
 
 from app.models import Job
 from app.database import SessionLocal, engine
@@ -20,6 +36,52 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 templates = Jinja2Templates(directory="app/templates")
 
+
+class ThreadedGenerator:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        item = self.queue.get()
+        if item is StopIteration:
+            raise item
+        return item
+
+    def send(self, data):
+        self.queue.put(data)
+
+    def close(self):
+        self.queue.put(StopIteration)
+
+class ChainStreamHandler(StreamingStdOutCallbackHandler):
+    def __init__(self, gen):
+        super().__init__()
+        self.gen = gen
+
+    def on_llm_new_token(self, token: str, **kwargs):
+        self.gen.send(f"data: {token}\n\n")
+
+def chat_response_thread(g, prompt):
+    try:
+        chat = ChatOpenAI(
+            verbose=True,
+            streaming=True,
+            callback_manager=CallbackManager([ChainStreamHandler(g)]),
+            temperature=0.7,
+            openai_api_key=os.environ.get("OPENAI_API_KEY")
+        )
+        chat([SystemMessage(content="You are a poetic assistant"), HumanMessage(content=prompt)])
+
+    finally:
+        g.close()
+
+def generate_chat_response(prompt):
+    g = ThreadedGenerator()
+    threading.Thread(target=chat_response_thread, args=(g, prompt)).start()
+    return g
 
 @app.get("/")
 async def index(request: Request, db: Session = Depends(get_db)):
@@ -42,6 +104,15 @@ async def job_detail(job_id: int, request: Request):
         "job_detail.html", {"request": request, "job": job}
     )
 
+@app.get("/question-stream")
+async def stream_chat_response(message: str):
+    print(f"message: {message}")
+    if not message:
+        return
+    return StreamingResponse(
+        generate_chat_response(message),
+        media_type='text/event-stream'
+    )
 
 if __name__ == "__main__":
     import uvicorn
