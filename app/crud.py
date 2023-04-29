b43115a2ec7bbd3e42e31608f7694267f1e49d6a
h-k-nyosu <kuro.h1020@gmail.com>
2023-04-29 10:41:12 +0900

    wip: query builder作成

--- app/crud.py
+++ app/crud.py
@@ -1,7 +1,7 @@
 from typing import List
 from .models import Job
+from sqlalchemy import or_, desc, asc
 from sqlalchemy.orm import Session
-
 
 def get_job(db: Session, job_id: int):
     return db.query(Job).filter(Job.id == job_id).first()
@@ -9,3 +9,42 @@ def get_job(db: Session, job_id: int):
 
 def get_jobs(db: Session):
     return db.query(Job).all()
+
+
+def get_custom_jobs(db: Session, query_params: dict):
+    query = db.query(Job)
+
+    # Filter
+    if "keyword" in query_params:
+        keyword = query_params["keyword"]
+        query = query.filter(
+            or_(
+                Job.title.ilike(f"%{keyword}%"),
+                Job.job_type.ilike(f"%{keyword}%"),
+                Job.job_summary.ilike(f"%{keyword}%"),
+                Job.job_details.ilike(f"%{keyword}%"),
+                Job.location.ilike(f"%{keyword}%"),
+            )
+        )
+
+    if "job_type" in query_params:
+        query = query.filter(Job.job_type == query_params["job_type"])
+
+    if "location" in query_params:
+        query = query.filter(Job.location == query_params["location"])
+
+    if "min_salary" in query_params:
+        query = query.filter(Job.monthly_salary >= query_params["min_salary"])
+
+    if "max_salary" in query_params:
+        query = query.filter(Job.monthly_salary <= query_params["max_salary"])
+
+    # Sort
+    if "sort_by" in query_params:
+        order = query_params.get("order", "asc")
+        if order == "asc":
+            query = query.order_by(asc(getattr(Job, query_params["sort_by"])))
+        elif order == "desc":
+            query = query.order_by(desc(getattr(Job, query_params["sort_by"])))
+
+    return query.all()
