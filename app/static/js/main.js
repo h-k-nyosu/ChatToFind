359e5baef3a4350549e4217cd4863cbaf9681324
h-k-nyosu <kuro.h1020@gmail.com>
2023-04-30 08:47:51 +0900

    検索結果のデザイン調整

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -71,6 +71,41 @@ function responseAiMessage(message) {
   };
 }
 
+function renderItem(job) {
+  return `
+        <a href="/jobs/${job.id}">
+          <div class="item">
+            <p class="item-job-type">#${job.job_type}</p>
+            <h3 class="item-job-title">${job.title}</h3>
+            <p class="item-job-monthly-salary">月給：${job.monthly_salary}円</p>
+            <p class="item-job-location">勤務地：${job.location}</p>
+          </div>
+        </a>
+      `;
+}
+
+function renderSearchResult(searchResult) {
+  if (searchResult.search_results.length === 0) {
+    return "";
+  }
+
+  const searchResultItems = searchResult.search_results
+    .map(renderItem)
+    .join("");
+
+  return `
+        <h2 class="search-title">${searchResult.title}</h2>
+        <div class="search-results">
+          ${searchResultItems}
+        </div>
+      `;
+}
+
+function renderMainContent(searchResults) {
+  const mainContent = document.querySelector(".main-content");
+  mainContent.innerHTML = searchResults.map(renderSearchResult).join("");
+}
+
 function fetchSearchItems(message) {
   console.log("fetchSearchItems start");
   fetch(`/search-items?message=${encodeURIComponent(message)}`)
@@ -87,6 +122,7 @@ function fetchSearchItems(message) {
         "searchResults.search_results: " + searchResults.search_results
       );
       console.log("fetchSearchItems finish");
+      renderMainContent(searchResults);
     })
     .catch((error) => {
       console.error(error);
