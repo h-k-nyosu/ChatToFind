6d2247becec1da1da5e61cb77740b5c51c035f9a
h-k-nyosu <kuro.h1020@gmail.com>
2023-05-02 08:49:29 +0900

    複数jsonをparseできるように変更

--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,14 @@
-from jsonfinder import jsonfinder
+import json
+import re
 
 
-def parse_json(text):
-    res_jsonfinder = jsonfinder(text)
-    res_json = []
-    for res in res_jsonfinder:
-        if res[2]:
-            res_json.append(res[2])
-    return res_json
+def split_json_objects(text: str):
+    pattern = re.compile(r"```json\s+({.*?})\s+```", re.DOTALL)
+    return pattern.findall(text)
+
+
+def parse_json(text: str):
+    json_objects_str = split_json_objects(text)
+    json_list = [json.loads(json_object_str) for json_object_str in json_objects_str]
+
+    return json_list
